f733d7ebb517d6177eff42ecc6e9847fe6423015
Killam-Git <zaidmallik9@gmail.com>
2022-08-20 14:27:40 +0530

    Register Page CSS Fix

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,14 +2,14 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
-
+from .models import CustomUser, School
 
 
 class RegistrationForm(UserCreationForm):
     class Meta:
-        model = User
+        model = CustomUser
         first_name = forms.CharField(max_length=30)
         last_name = forms.CharField(max_length=30)
-        school = forms.Select()
-        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+        school = forms.ModelChoiceField(queryset=School.objects.all())
+        fields = ('first_name', 'last_name', 'school', 'username', 'email', 'password1', 'password2')
         
